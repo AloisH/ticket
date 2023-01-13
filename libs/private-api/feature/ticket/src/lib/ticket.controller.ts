bf756e628e0384d360fd178387f04088f2d5fa9b
Alois Heloir <alois.heloir@live.fr>
2023-01-13 11:18:28 +0100

    feat(ticket): Add create in controllert

--- libs/private-api/feature/ticket/src/lib/ticket.controller.ts
+++ libs/private-api/feature/ticket/src/lib/ticket.controller.ts
@@ -1,5 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { UserId } from '@ticket/private-api/core/auth';
+import { CreateTicketDto } from './dto/create-ticket.dto';
 import { TicketService } from './ticket.service';
 
+@Controller('ticket')
 export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
+
+  @Post()
+  public async create(@UserId() userId: number, @Body() createTicketDto: CreateTicketDto) {
+    return;
+  }
 }
--- libs/private-api/feature/ticket/src/lib/ticket.service.ts
+++ libs/private-api/feature/ticket/src/lib/ticket.service.ts
@@ -1,5 +1,13 @@
+import { CreateTicketDto } from './dto/create-ticket.dto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
 export class TicketService {
   constructor() {
+    return;
+  }
+
+  public async createDto(userId: number, createTicketDto: CreateTicketDto) {
     return;
   }
 }
